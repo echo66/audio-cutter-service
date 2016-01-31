b83ed11af1e9fe2f304a93fd9ed3fb79a7b3766a
echo66 <bruno.filipe.silva.dias@gmail.com>
2016-01-31 16:57:55 +0000

    Added app.js to make a demo deployment easier. Refactored audio-cutter-service.js

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var cors = require('cors');
+var log4js = require('log4js');
+var cutter = require('./audio-cutter-service.js');
+
+var logger = log4js.getLogger();
+var app = express();
+app.use(cors());
+app.use('/', cutter.router);
+
+var server = app.listen(100, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	logger.info('Audio cutter server listening at http://%s:%s', host, port);
+});
--- audio-cutter-service.js
+++ audio-cutter-service.js
@@ -2,32 +2,44 @@ var ffmpeg = require('fluent-ffmpeg');
 var fs = require('fs');
 var router = require('express').Router();
 
+function cut_to_pipe(filepath, format, start, duration, output) {
+  ffmpeg(filepath)
+      .on('error', function(err) {
+            console.log('Processing error! ' + err);
+      })
+      .format(format)
+      .audioCodec('copy')
+      .seekInput(parseFloat(start)).duration(parseFloat(duration))
+      .pipe(output, { end : true });
+
+
+}
+
 router.get ('/', function(req,res) {
   
-  var path = req.query.path;
-  console.log(path);
-  if (path == undefined)
+  var url = req.query.url;
+
+  console.log(url);
+
+  if (url == undefined)
     res.status(500);
 
-  ffmpeg.ffprobe(path, function(err, metadata) {
+  ffmpeg.ffprobe(url, function(err, metadata) {
     if (err) 
       res.status(500);
 
     res.writeHead(200, {
       'Content-Type': 'audio/mpeg',
     });
-    
-    ffmpeg(path)
-      .on('error', function(err) {
-            console.log('Processing error! ' + err);
-      })
-      .format(metadata.format.format_name)
-      .audioCodec('copy')
-      .seekInput(parseFloat(req.query.start)).duration(parseFloat(req.query.duration))
-      .pipe(res, {end:true});
+
+    cut_to_pipe(url, metadata.format.format_name, req.query.start, req.query.duration, res);
+
   });
            
 });
 
 
-module.export = router;
+module.exports = {
+  router : router, 
+  cut_to_pipe : cut_to_pipe
+};
